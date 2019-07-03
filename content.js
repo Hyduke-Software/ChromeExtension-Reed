46564d49a2d723bc5c6f570d5cb0619484001589
Hyduke-Software <gallimore.tj@gmail.com>
2019-07-03 15:48:42 +0100

    Add files via upload
    Working with several text scripts.
    
    Only performing 'warning mode'

--- content.js
+++ content.js
@@ -2,16 +2,21 @@
 
 //This version only works on reed.co.uk
 
+var searchterms = ["successful completion of the training course","The Trainee"]
 var queryresults = document.querySelectorAll('[id^="jobSection"]');
+
 
 for (var i = 0; i < queryresults.length; i++) { console.log( queryresults[i].textContent) //loops through all 
 
-var pos = queryresults[i].textContent.search("The Trainee")
+		for (var j = 0; j < searchterms.length; j++) { //searches the fields for the text in the array searchterms
+			var pos = queryresults[i].textContent.search(searchterms[j])
 	if (pos > -1){
-		alert ("Removing a pretend job "+i)
-queryresults[i].style.backgroundColor = "red";  //changes the class to red colour
-queryresults[i].remove(); //remmoves jobSectioni from the browser
+
+		queryresults[i].style.backgroundColor = "red";  //changes the class to red colour
 
 	}
-
+		}
+		//
+		
 ;}
+//was 
